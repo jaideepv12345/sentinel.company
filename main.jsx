685af9e24e262d2e4223f6e942517cab8dc8ab0e
jaideepv12345 <jkvisave@uncg.edu>
2026-03-19 12:51:36 -0400

    Replace App component with SOPSentinel component

--- main.jsx
+++ main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './SOP-Sentinel-v5.jsx' // Make sure this matches your filename
-import './index.css' // If you have a CSS file
+// ... All your imports from the SOP file stay here ...
 
+const SOPSentinel = () => {
+  // ... All your existing app logic stays here ...
+};
+
+// CRITICAL: This must be at the very bottom of main.jsx
+import ReactDOM from 'react-dom/client';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SOPSentinel />
+  </React.StrictMode>
+);
