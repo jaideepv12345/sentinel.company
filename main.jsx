1e27c855bcceb890881903376e19ff0a43f02257
jaideepv12345 <jkvisave@uncg.edu>
2026-03-19 12:46:21 -0400

    Fix import statements and add newline at end of file

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './SOP-Sentinel-v5.jsx'
-import './index.css'
+import App from './SOP-Sentinel-v5.jsx' // Make sure this matches your filename
+import './index.css' // If you have a CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
